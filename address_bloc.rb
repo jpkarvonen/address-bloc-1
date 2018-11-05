c3d4e2adb226cf9d7dd35ded1a2801ab81652dd6
John <jpkarvonen@gmail.com>
2018-11-05 15:27:58 -0500

    assignment complete

--- address_bloc.rb
+++ address_bloc.rb
@@ -2,7 +2,7 @@ require_relative 'controllers/menu_controller'
 require '/home/john/bloc/bloc_record/lib/bloc_record'
 
 # Choose preferred database platform:
-BlocRecord.connect_to("db/address_bloc.sqlite")
+BlocRecord.connect_to("db/address_bloc.sqlite", :sqlite3)
 # BlocRecord.connect_to("db/address_bloc.db", :pg)
 
 menu = MenuController.new
